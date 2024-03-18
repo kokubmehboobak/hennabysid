8e87dfc82beda155448d6b2cf5f908ecff1f8746
Kokub Mehboob <kokub302@gmail.com>
2024-03-18 12:20:25 +0500

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,36 @@
+ <!-- ======= Footer ======= -->
+ <footer id="footer" class="footer">
+    <div class="container">
+       
+      <div class="copyright">
+        <div class="header-social-links">
+            <a href="#" class="twitter"><i class="bi bi-tiktok"></i></a> &nbsp;
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a> &nbsp;
+            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a> &nbsp;
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+          </div>
+        &copy; Copyright <strong><span>AK</span></strong>. All Rights Reserved
+      </div>
+     
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader">
+    <div class="line"></div>
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/aos/dist/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+
+  <script src="assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Henna By Sid</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Cardo:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+ 
+  <!-- Vendor CSS Files -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/aos/dist/aos.css" rel="stylesheet">
+
+<link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+
+      <a href="/" class="logo d-flex align-items-center  me-auto me-lg-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <i class="bi bi-sun"></i>
+        <h1>Henna By Sid</h1>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="/" class="active">Home</a></li>
+          <li><a href="about">About</a></li>
+          <li class="dropdown"><a href="#"><span>Gallery</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="gallery">Cultural</a></li>
+              <li><a href="gallery">Modern Mandalas</a></li>
+              <li><a href="gallery">Traditional Treasures</a></li>
+              <li><a href="gallery">Finger Finery</a></li>
+              <li><a href="gallery">Full Arm Flourish</a></li>
+              <li><a href="gallery">Minimalist Magic</a></li>
+              <li class="dropdown"><a href="#"><span>Handcrafted Henna</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                  <li><a href="gallery">Front Hand Finesse</a></li>
+                  <li><a href="gallery">Back Hand Beauty</a></li>
+                  <li><a href="gallery">Tikki Tales</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href="services">Services</a></li>
+          <li><a href="contact">Contact</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links">
+        <a href="#" class="twitter"><i class="bi bi-tiktok"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+      </div>
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+    </div>
+  </header><!-- End Header -->
+
+  
